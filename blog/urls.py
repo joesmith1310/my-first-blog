4c95f02fa51d2b7a72bdfeb4adc40134ccfe0763
Joe Smith <joe@Joes-MacBook-Pro.local>
2020-08-19 16:44:06 +0100

    Fixed static files not loading.

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
@@ -7,3 +8,5 @@ urlpatterns = [
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
